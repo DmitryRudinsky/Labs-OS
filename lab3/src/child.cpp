5871be6518a784b446e8063b107af47d214982fa
DmitryRudinsky <guladmitriy1331@gmail.com>
2024-12-19 19:20:07 +0300

    3 fix

--- lab3/src/child.cpp
+++ lab3/src/child.cpp
@@ -8,7 +8,8 @@
 
 const int SHM_SIZE = 4096;
 const char* SHM_NAME = "/my_shm";
-const char* SEM_NAME = "/my_sem";
+const char* DATA_SEM_NAME = "/my_data_sem";
+const char* PROCESSING_SEM_NAME = "/my_processing_sem";
 
 int main() {
     int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
@@ -23,14 +24,20 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    sem_t* sem = sem_open(SEM_NAME, 0);
-    if (sem == SEM_FAILED) {
-        perror("sem_open error");
+    sem_t* data_sem = sem_open(DATA_SEM_NAME, 0);
+    if (data_sem == SEM_FAILED) {
+        perror("sem_open data_sem error");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_t* processing_sem = sem_open(PROCESSING_SEM_NAME, 0);
+    if (processing_sem == SEM_FAILED) {
+        perror("sem_open processing_sem error");
         exit(EXIT_FAILURE);
     }
 
     while (true) {
-        sem_wait(sem);
+        sem_wait(data_sem);
 
         if (shm_ptr[0] == '\0') {
             break;
@@ -50,11 +57,12 @@ int main() {
             line = strtok(nullptr, "\n");
         }
 
-        sem_post(sem);
+        sem_post(processing_sem);
     }
 
     munmap(shm_ptr, SHM_SIZE);
-    sem_close(sem);
+    sem_close(data_sem);
+    sem_close(processing_sem);
 
     return 0;
 }
--- lab3/src/parent.cpp
+++ lab3/src/parent.cpp
@@ -8,7 +8,8 @@
 
 const int SHM_SIZE = 4096;
 const char* SHM_NAME = "/my_shm";
-const char* SEM_NAME = "/my_sem";
+const char* DATA_SEM_NAME = "/my_data_sem";
+const char* PROCESSING_SEM_NAME = "/my_processing_sem";
 
 int main() {
     char filename[256];
@@ -33,9 +34,15 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    sem_t* sem = sem_open(SEM_NAME, O_CREAT, 0666, 0);
-    if (sem == SEM_FAILED) {
-        perror("sem_open error");
+    sem_t* data_sem = sem_open(DATA_SEM_NAME, O_CREAT, 0666, 0);
+    if (data_sem == SEM_FAILED) {
+        perror("sem_open data_sem error");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_t* processing_sem = sem_open(PROCESSING_SEM_NAME, O_CREAT, 0666, 0);
+    if (processing_sem == SEM_FAILED) {
+        perror("sem_open processing_sem error");
         exit(EXIT_FAILURE);
     }
 
@@ -57,19 +64,23 @@ int main() {
         ssize_t bytesRead;
         while ((bytesRead = read(file_fd, shm_ptr, SHM_SIZE - 1)) > 0) {
             shm_ptr[bytesRead] = '\0';
-            sem_post(sem);
-            sem_wait(sem);
+            sem_post(data_sem);
+            sem_wait(processing_sem);
         }
 
+        shm_ptr[0] = '\0';
+        sem_post(data_sem);
+
         close(file_fd);
-        sem_post(sem);
 
         waitpid(pid, nullptr, 0);
 
         munmap(shm_ptr, SHM_SIZE);
         shm_unlink(SHM_NAME);
-        sem_close(sem);
-        sem_unlink(SEM_NAME);
+        sem_close(data_sem);
+        sem_unlink(DATA_SEM_NAME);
+        sem_close(processing_sem);
+        sem_unlink(PROCESSING_SEM_NAME);
     }
 
     return 0;
