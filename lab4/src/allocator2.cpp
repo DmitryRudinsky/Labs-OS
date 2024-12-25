698578629206ef54116e2a29c8bb028aa09c5ee6
DmitryRudinsky <guladmitriy1331@gmail.com>
2024-12-25 23:09:39 +0300

    fix 4

--- lab4/src/allocator2.cpp
+++ lab4/src/allocator2.cpp
@@ -17,6 +17,15 @@ public:
         for (size_t i = 0; i < NUM_SIZES; ++i) {
             free_lists[i].reserve(sz / BLOCK_SIZES[i]);
         }
+
+        for (size_t i = 0; i < NUM_SIZES; ++i) {
+            size_t block_size = BLOCK_SIZES[i];
+            size_t num_blocks = sz / block_size;
+            for (size_t j = 0; j < num_blocks; ++j) {
+                void* block = (char*)memory + j * block_size;
+                free_lists[i].push_back(block);
+            }
+        }
     }
 
     ~Allocator2() {
@@ -30,10 +39,10 @@ public:
                     free_lists[i].pop_back();
                     return ptr;
                 } else {
-                    if ((char*)memory + BLOCK_SIZES[i] <= (char*)memory + size) {
+                    if ((char*)memory + BLOCK_SIZES[i] <= (char*)memory + this->size) {
                         void* ptr = memory;
                         memory = (char*)memory + BLOCK_SIZES[i];
-                        size -= BLOCK_SIZES[i];
+                        this->size -= BLOCK_SIZES[i];
                         return ptr;
                     }
                 }
@@ -46,7 +55,7 @@ public:
         if (!ptr) return;
 
         for (size_t i = 0; i < NUM_SIZES; ++i) {
-            if (ptr >= memory && (char*)ptr < (char*)memory + size) {
+            if (BLOCK_SIZES[i] >= size) {
                 free_lists[i].push_back(ptr);
                 return;
             }
@@ -55,19 +64,19 @@ public:
 };
 
 extern "C" {
-    Allocator2* allocator2_create(void* memory, size_t size) {
+    Allocator2* allocator_create(void* memory, size_t size) {
         return new (memory) Allocator2(memory, size);
     }
 
-    void allocator2_destroy(Allocator2* allocator) {
+    void allocator_destroy(Allocator2* allocator) {
         allocator->~Allocator2();
     }
 
-    void* allocator2_alloc(Allocator2* allocator, size_t size) {
+    void* allocator_alloc(Allocator2* allocator, size_t size) {
         return allocator->alloc(size);
     }
 
-    void allocator2_free(Allocator2* allocator, void* ptr) {
+    void allocator_free(Allocator2* allocator, void* ptr) {
         allocator->free(ptr);
     }
 }
--- lab4/src/main.cpp
+++ lab4/src/main.cpp
@@ -2,6 +2,8 @@
 #include <dlfcn.h>
 #include <cstdlib>
 #include <ctime>
+#include <vector>
+#include <chrono>
 
 typedef void* (*CreateFunc)(void*, size_t);
 typedef void (*DestroyFunc)(void*);
@@ -43,17 +45,60 @@ int main(int argc, char* argv[]) {
     void* allocator_memory = malloc(allocator_size);
     void* allocator = create(allocator_memory, allocator_size);
 
+    // Тест 1: Базовое выделение и освобождение
     void* ptr1 = alloc(allocator, 100);
     void* ptr2 = alloc(allocator, 200);
 
     if (ptr1 && ptr2) {
         std::cout << "Allocation successful!" << std::endl;
+        std::cout << "ptr1: " << ptr1 << " (100 bytes)" << std::endl;
+        std::cout << "ptr2: " << ptr2 << " (200 bytes)" << std::endl;
     } else {
         std::cerr << "Allocation failed!" << std::endl;
+        if (!ptr1) std::cerr << "Failed to allocate 100 bytes" << std::endl;
+        if (!ptr2) std::cerr << "Failed to allocate 200 bytes" << std::endl;
     }
 
     free_func(allocator, ptr1);
     free_func(allocator, ptr2);
+
+    // Тест 2: Выделение и освобождение множества блоков
+    std::vector<void*> pointers;
+    for (int i = 0; i < 100; ++i) {
+        void* ptr = alloc(allocator, 128);
+        if (ptr) {
+            pointers.push_back(ptr);
+        } else {
+            std::cerr << "Failed to allocate block " << i << std::endl;
+            break;
+        }
+    }
+
+    for (void* ptr : pointers) {
+        free_func(allocator, ptr);
+    }
+
+    // Тест 3: Проверка на утечки памяти
+    size_t allocated_memory = 0;
+    size_t freed_memory = 0;
+
+    void* ptr3 = alloc(allocator, 300);
+    if (ptr3) allocated_memory += 300;
+
+    void* ptr4 = alloc(allocator, 400);
+    if (ptr4) allocated_memory += 400;
+
+    free_func(allocator, ptr3);
+    freed_memory += 300;
+
+    free_func(allocator, ptr4);
+    freed_memory += 400;
+
+    if (allocated_memory == freed_memory) {
+        std::cout << "No memory leaks detected!" << std::endl;
+    } else {
+        std::cerr << "Memory leak detected!" << std::endl;
+    }
 
     destroy(allocator);
     free(allocator_memory);
